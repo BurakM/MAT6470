cc8f987ed5995bf06dbfabb31da7a79e0ca99b71
BurakM <moralib@gmail.com>
2016-01-22 12:13:43 -0500

    Fix Mistakes

--- TP2.m
+++ TP2.m
@@ -1,25 +1,29 @@
 % Burak Morali - Alexandre Carbonneau
 % TP2 - MAT6470
 
+clc;clear
+
 % Constantes
 B=0.8;a=4;p0=0.5;A=4*B;q=1.6;
 
 % Conditions frontieres
-initix=0.001;
+initix=0.01;
 initip=0;
 
 % Resolution algebrique avec Matlab
 
 syms x(t) p(t)
-eqn1 = diff(x) == 2*A*(sinh(p)+q*x-x*cosh(p+q*x));
-eqn2 = diff(p) == -2*B*(p0*sinh(a*x+p*cosh(a*x)));  
+eqn1 = diff(x) == 2*A*(sinh(p+q*x)-x*cosh(p+q*x));
+eqn2 = diff(p) == -2*B*(p0*sinh(a*x)+p*cosh(a*x));  
 S = dsolve(eqn1, eqn2);
 
 % Resolution numerique
 
-options = odeset('RelTol',1e-4,'AbsTol',[1e-4 1e-4 1e-5]);
-[T,Y] = ode45(@rigid,[0 12],[initix initip]);
+% options = odeset('RelTol',1e-4,'AbsTol',[1e-4 1e-4 1e-5]);
 
 
-plot(T,Y)
+[T,Y45] = ode45(@rigid,[0 10],[initix initip]);
+
+
+plot(T,Y45)
 
